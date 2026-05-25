93af36ea80dc17690a5dc805b50ee50ee7072b74
Chris Kim <chkim@tuta.io>
2026-05-24 21:20:07 -0700

    cleaning

--- src/features/__init__.py
+++
@@ -1,2 +0,0 @@
-from src.features.wallet_features import build_features
-from src.features.scaling import scale_features
--- src/models/__init__.py
+++
@@ -1,6 +0,0 @@
-from src.models.clustering import (
-    sweep_kmeans, fit_kmeans, fit_dbscan, fit_isolation_forest,
-)
-from src.models.evaluation import (
-    cluster_profiles, cluster_sizes, umap_embed, top_wallets_per_cluster,
-)
